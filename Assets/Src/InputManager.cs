f9cc7f24c672f025398985a78e904e4299e28065
Samuli Kinnunen <samuli.kinnunen@movya.fi>
2023-02-04 07:31:50 +0200

    Safe commit(?)

---
+++ Assets/Src/InputManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GyroKame
+{
+    public class InputManager : MonoBehaviour
+    {
+        private void Start()
+        {
+            
+        }
+    }
+
+}
