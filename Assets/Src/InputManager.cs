3beb52ef012ba1384caeaf994b14a86b912f6303
Samuli Kinnunen <samuli.kinnunen@movya.fi>
2023-02-04 08:33:02 +0200

    Stashing

--- Assets/Src/InputManager.cs
+++ Assets/Src/InputManager.cs
@@ -6,9 +6,23 @@ namespace GyroKame
 {
     public class InputManager : MonoBehaviour
     {
+        [SerializeField] private TMPro.TextMeshProUGUI shitbox;
         private void Start()
         {
-            
+            Application.logMessageReceived += Application_logMessageReceived;
+
+            if (UnityEngine.InputSystem.Accelerometer.current == null)
+            {
+                Debug.LogError("no acc");
+            } else
+            {
+                Debug.Log("Accelerometer!");
+            }
+        }
+
+        private void Application_logMessageReceived(string condition, string stackTrace, LogType type)
+        {
+            shitbox.text += "\n" + condition;
         }
     }
 
